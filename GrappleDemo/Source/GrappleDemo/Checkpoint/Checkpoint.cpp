9c700d8d0dd2c1bee76f5cf46edbb51d3d187f44
BaseDorp <44095612+BaseDorp@users.noreply.github.com>
2021-03-10 02:25:18 -0600

    changes to scene and fixes reticle error

---
+++ GrappleDemo/Source/GrappleDemo/Checkpoint/Checkpoint.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Checkpoint.h"
+
+// Sets default values
+ACheckpoint::ACheckpoint()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ACheckpoint::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ACheckpoint::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
